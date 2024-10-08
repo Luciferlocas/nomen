7111bfdf3b08238d27e2c88a481ef14537a4ca7b
atulgupta0403 <atulgupta0403@gmail.com>
2024-10-08 16:44:24 +0530

    added resume

---
+++ app/_content/contributors/atul-gupta.ts
@@ -0,0 +1,65 @@
+export const data = {
+    name: "ATUL GUPTA",
+    location: "Ghaziabad , Uttar Pradesh",
+    phone: "6393812084",
+    email: "atulgupta0403@gmail.com",
+    links: {
+      linkedin: "https://www.linkedin.com/in/atul-gupta-3250292b0/",
+      github: "https://github.com/atulgupta0403",
+      leetcode: "https://leetcode.com/u/atulgupta0403/",
+      // codeforces: "YOUR CODEFORCES PROFILE",
+    },
+    education: [
+      {
+        institution: "Ajay Kumar Garg Engineering College",
+        location: "Ghaziabad , IN",
+        degree: "Bachelor of Technology - Computer Science and Engineering",
+        period: "2023 – 2027",
+      },
+    ],
+    experience: [
+      {
+        company: "Block Chain research Lab | trannie",
+        position: "Backend Developer",
+        location: "Ghaziabad , Uttar Pradesh",
+        period: "sept. 2024 – present",
+        responsibilities: [
+          "Complete the task given by seniors",
+          // "RESPONSIBILITY 2",
+          // "RESPONSIBILITY 3",
+        ],
+      },
+      // {
+      //   company: "ANOTHER COMPANY",
+      //   position: "ANOTHER POSITION",
+      //   location: "LOCATION",
+      //   period: "START DATE – END DATE",
+      //   responsibilities: [
+      //     "RESPONSIBILITY 1",
+      //     "RESPONSIBILITY 2",
+      //     "RESPONSIBILITY 3",
+      //   ],
+      // },
+    ],
+    projects: [
+      {
+        name: "E-commerce",
+        technologies: "React",
+        period: "1month",
+        // details: ["DETAIL 1", "DETAIL 2", "DETAIL 3"],
+      },
+      // {
+      //   name: "ANOTHER PROJECT",
+      //   technologies: "TECHNOLOGIES USED",
+      //   period: "PROJECT DURATION",
+      //   details: ["DETAIL 1", "DETAIL 2", "DETAIL 3"],
+      // },
+    ],
+    skills: {
+      languages: ["JavaScript", "java"],
+      technologies: ["Reactjs", "Nodejs", "Expressjs"],
+      // tools: ["TOOL 1", "TOOL 2"],
+    },
+    achievements: ["ACHIEVEMENT 1", "ACHIEVEMENT 2", "ACHIEVEMENT 3"],
+  };
+  
