a2d845b6f2d4b667b97a5fdcae69a4753d68046a
Khushi Roy <roykhushi432@gmai.com>
2024-10-09 20:39:57 +0530

    changed strikethrough in explore button and UI changes in the main and subtext

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col">
-      <h2 className="text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-neutral-500 text-center">
+      <h2 className="text-3xl md:text-5xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-center">
         Welcome to <br />
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-white via-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-600 to-blue-500">
           NomeN
         </span>
       </h2>
-      <p className="max-w-5xl mx-auto text-sm md:text-lg text-neutral-400 text-center">
+      <p className="max-w-4xl mx-auto text-sm md:text-lg lg:text-xl text-neutral-400 text-center mt-4 lg:mt-6 leading-relaxed">
         {
           "It doesn't merely build your resume, but rather crafts a legendary professional legacy that transforms your career into a mesmerizing narrative that demands to be read and remembered."
         }
