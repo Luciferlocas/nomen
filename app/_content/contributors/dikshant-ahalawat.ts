376785f6419194eb5db6223feb7169f0959c74e0
Dikshant <dikshantahalawat2005@gmail.com>
2024-10-10 17:21:26 +0530

    uncommented stuff

--- app/_content/contributors/dikshant-ahalawat.ts
+++ app/_content/contributors/dikshant-ahalawat.ts
@@ -7,6 +7,7 @@ export const data = {
       linkedin: "https://www.linkedin.com/in/dikshant-ahalawat-6a9375293/",
       github: "https://github.com/Dikshant005/",
       leetcode: "https://leetcode.com/u/Dikshant_Ahalawat/",
+      codeforces: "NONE",
       
     },
     education: [
@@ -17,36 +18,42 @@ export const data = {
         period: "2023 – 2027",
       },
     ],
-    // experience: [
-    //   {
-    //     company: "YOUR COMPANY",
-    //     position: "YOUR POSITION",
-    //     location: "LOCATION",
-    //     period: "START DATE – END DATE",
-    //     responsibilities: [
-    //       "RESPONSIBILITY 1",
-    //       "RESPONSIBILITY 2",
-    //       "RESPONSIBILITY 3",
-    //     ],
-    //   },
-    //   {
-    //     company: "ANOTHER COMPANY",
-    //     position: "ANOTHER POSITION",
-    //     location: "LOCATION",
-    //     period: "START DATE – END DATE",
-    //     responsibilities: [
-    //       "RESPONSIBILITY 1",
-    //       "RESPONSIBILITY 2",
-    //       "RESPONSIBILITY 3",
-    //     ],
-    //   },
-    // ],
+    experience: [
+      {
+        company: "CHESS BASE INDIA",
+        position: "END GAME MASTER",
+        location: "GHAZIABAD",
+        period: "2013 – 2024",
+        responsibilities: [
+          "TEAM HANDLING",
+          "WINNING",
+          "PERFORMING IN TOURNAMENTS",
+        ],
+      },
+      {
+        company: "ANOTHER COMPANY",
+        position: "ANOTHER POSITION",
+        location: "LOCATION",
+        period: "START DATE – END DATE",
+        responsibilities: [
+          "RESPONSIBILITY 1",
+          "RESPONSIBILITY 2",
+          "RESPONSIBILITY 3",
+        ],
+      },
+    ],
     projects: [
       {
         name: "Authentication_App",
         technologies: " FLUTTER,FIREBASE",
         period: "7 DAYS",
         details: ["LETS THE USER SIGNUP AND SIGN IN WITH EMAIL PASSWORD AND GOOGLE SIGN IN AND THE USER CAN RESET HIS PASSWORD"],
+      },
+      {
+        name: "ANOTHER PROJECT",
+        technologies: "TECHNOLOGIES USED",
+        period: "PROJECT DURATION",
+        details: ["DETAIL 1", "DETAIL 2", "DETAIL 3"],
       },
       
     ],
