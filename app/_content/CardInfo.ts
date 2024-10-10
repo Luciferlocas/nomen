f9cd6cc722c3f9b0b51348a3fe6f531ffa8f4979
Arpit Anand <invoker.arpit@gmail.com>
2024-10-10 10:31:12 +0530

    Arpit Anand Added Resume

--- app/_content/CardInfo.ts
+++ app/_content/CardInfo.ts
@@ -49,4 +49,14 @@ export const info = [
     avatar: "https://avatars.githubusercontent.com/u/168955332?v=4",
     background: "https://images.unsplash.com/photo-1647866873870-92d45763ef44?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
+  {
+    name: "Arpit Anand",
+    slug: "Arpit-Anand",
+    domain: "Backend Dev",
+    description: "Open source contributor",
+    quote: "Imposter syndrome is the constant truth",
+    about: "I'm a BackendDev with a passion for open source projects.",
+    avatar: "https://avatars.githubusercontent.com/u/118847706?v=4",
+    background: "https://images.unsplash.com/photo-1647866873870-92d45763ef44?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
 ];
