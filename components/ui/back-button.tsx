7646a4174288c5a440e5cfd33532de67b94b0bd6
Luciferlocas <rohitsomvanshi2003@gmail.com>
2024-10-11 20:32:45 +0530

    set a minimum w for cards

--- components/ui/back-button.tsx
+++ components/ui/back-button.tsx
@@ -8,7 +8,7 @@ const BackButton = () => {
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
       </span>
       <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-1 px-1 ring-1 ring-white/10 ">
-        <span className="hover:line-through"><BackArrow /></span>
+        <span className="p-[3px]"><BackArrow /></span>
       </div>
     </button>
   );
