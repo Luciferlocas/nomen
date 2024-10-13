4ffb0861db2787b660e84d8484a8a542c705da67
Sarthak Choudhary <2405834@kiit.ac.in>
2024-10-13 12:40:45 +0000

    Divided rows equally

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,14 +8,17 @@ import SpecialButton from "@/components/ui/special-button";
 import BackArrow from "@/components/icons/BackArrow";
 
 const page = () => {
+  const halfLength = Math.ceil(info.length / 2);
+  const firstHalf = info.slice(0, halfLength);
+  const secondHalf = info.slice(halfLength);
   return (
     <div>
         <Link href="/" className="fixed left-4 top-4 z-[9999] ">
           <SpecialButton className="p-[6px]" classButton="mt-0" icon={<BackArrow/>}/>
         </Link>
       <div className="min-h-screen rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-        <InfiniteMovingCards data={info} direction="right" speed="fast" />
-        <InfiniteMovingCards data={info} direction="left" speed="fast" />
+        <InfiniteMovingCards data={firstHalf} direction="right" speed="fast" />
+        <InfiniteMovingCards data={secondHalf} direction="left" speed="fast" />
 
         <TextRevealCard
           className="mt-8"
