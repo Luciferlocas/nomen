54e9195d4cf3c578ac5998645a0192f2758ade67
Khushi Roy <roykhushi432@gmai.com>
2024-10-14 21:21:52 +0530

    landing page changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col">
-      <h2 className="text-3xl md:text-5xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-center text-gray-300">
+      <h2 className="text-3xl md:text-5xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight text-center text-[#cfd3d9]">
         Welcome to <br />
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#323238] via-gray-500 to-[#323238]">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#2a628f] via-[#848f9a] to-[#2a628f]">
           NomeN
         </span>
       </h2>
-      <p className="max-w-4xl mx-auto text-sm md:text-lg lg:text-xl text-neutral-400 text-center mt-4 lg:mt-6 leading-relaxed">
+      <p className="max-w-4xl mx-auto text-sm md:text-lg lg:text-xl text-neutral-300/85 text-center mt-4 lg:mt-6 leading-relaxed">
         {
           "It doesn't merely build your resume, but rather crafts a legendary professional legacy that transforms your career into a mesmerizing narrative that demands to be read and remembered."
         }
