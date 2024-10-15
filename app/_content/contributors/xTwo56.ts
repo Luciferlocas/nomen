09472b054fb6ce05a0126ae099615a6a15985fe0
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-10-15 20:43:52 +0530

    new resume added

---
+++ app/_content/contributors/xTwo56.ts
@@ -0,0 +1,69 @@
+
+export const data = {
+  name: "xTwo56",
+  location: "Ghaziabad, UP",
+  phone: null,
+  email: "anonymous256@gmail.com",
+  links: {
+    github: "https://github.com/xTwo56",
+
+  },
+  education: [
+    {
+      institution: "Indian Institute of Informational Technology, Bhagalpur",
+      location: "Bhagalpur, Bihar",
+      degree: "Computer Science and Engineering",
+      period: "August. 2023 – May 2027"
+    },
+  ],
+  experience: [
+    {
+      company: "Quamin",
+      position: "Blockchain Developer",
+      period: "June - September",
+      responsibilities: [
+        "Analyzed and researched strategies to detect financial frauds",
+        "Assisted in development of efficient payment gateway system",
+      ],
+    },
+    {
+      company: "GirlScript Summer of Code",
+      position: "Open Source Contributor",
+      period: "May - August",
+      responsibilities: [
+        "Handled auth flow",
+        "UI/UX refinement"
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "Bucket",
+      technologies: "Next, Typescript, Tailwindcss",
+      period: "August 2024-Present",
+      details: [
+        "A user-friendly reselling website"
+      ],
+    },
+
+    {
+      name: "vote-ind",
+      technologies: "Ethers, Hardhat, Js",
+      period: "July 2024 - August 2024",
+      details: [
+        "A full-stack dApp to decentralize voting",
+      ],
+    },
+
+  ],
+  skills: {
+    languages: ["Typescript", "Solidity", "Rust", "C++"],
+    technologies: ["Next", "React", "Prisma", "Hardhat"],
+    tools: ["Git/GitHub", "Docker", "Linux"],
+  },
+
+  achievements: [
+    "Winner of OPCODE, an intra-college open source event",
+    "Team selected in SIH",
+  ],
+}
