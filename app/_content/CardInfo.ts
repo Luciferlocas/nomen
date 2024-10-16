e774340ed6f505f0112966ff4ff3bac448340008
VaiMan <145927299+Vai-Man@users.noreply.github.com>
2024-10-16 16:47:41 +0530

    mystuff

--- app/_content/CardInfo.ts
+++ app/_content/CardInfo.ts
@@ -10,6 +10,16 @@ export const info = [
     background: "https://images.unsplash.com/photo-1647866873870-92d45763ef44?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
+    name: "Vaibhav Manihar",
+    slug: "VaiMan",
+    domain: "Developer",
+    description: "I love to learn something new everyday",
+    quote: "Do good and throw it in the sea",
+    about: " Sophomore who loves to code and build things ",
+    avatar: "https://i.imgur.com/JcSw3NR.png",
+    background: "https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
     name: "Gaurav Singh",
     slug: "euclid-stellar",
     domain: "SDE",
---
+++ app/_content/contributors/VaiMan.ts
@@ -0,0 +1,54 @@
+export const data = {
+  name: "Vaibhav Manihar",
+  location: "Jodhpur",
+  phone: "| :)",
+  email: null,
+  links: {
+    linkedin: "https://www.linkedin.com/in/vaiman/",
+    github: "https://github.com/vai-man",
+    leetcode: null, // No Leetcode provided
+    codeforces: null, // No Codeforces provided
+  },
+  education: [
+    {
+      institution: "VIT-AP",
+      location: "Andhra Pradesh, India",
+      degree: "B.Tech",
+      period: "2023 – 2027",
+    },
+  ],
+  experience: [
+    {
+      company: "Machine Learning Club",
+      position: "Projects Team",
+      period: "2023 - 2024",
+      responsibilities: [
+        "Built projects and learned some machine learning concepts",
+      ],
+    },
+  ],
+  projects: [
+    {
+        name: "Stock Market Prediction",
+        technologies: "Python, Machine Learning",
+        period: "1 week",
+        details: [
+          "Developed a stock market prediction model using fbprophet and yfinance to forecast future stock prices. This project provided valuable insights into time-series analysis and machine learning.",
+        ],
+      },
+  
+  ],
+  
+  skills: {
+    languages: ["Python", "C++", "C", "Java", "JavaScript", "HTML", "CSS", "SQL"],
+    technologies: ["Git", "Docker", "AWS"],
+    tools: ["VSCode", "GitHub"],
+  },
+  
+  achievements: [
+    "Won an Ideathon conducted in college",
+    "Mentioned others on LinkedIn",
+  ],
+};
+
+
