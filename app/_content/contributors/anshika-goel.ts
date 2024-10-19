5d82e78c1c85227d5e36a001c6a4528eba1ba125
AnshikaGoel <11402anshikagoyal@gmail.com>
2024-10-19 04:42:07 -0700

    anshika-goel added resume

---
+++ app/_content/contributors/anshika-goel.ts
@@ -0,0 +1,65 @@
+export const data = {
+    name: "Anshika Goel",
+    location: "Ghazibad ,India",
+    phone: "9389529423",
+    email: "11402anshikagoyal@gmail.com",
+    links: {
+      linkedin: "https://www.linkedin.com/in/anshika-goel-25b537314/",
+      github: "https://github.com/Anshikagoel11",
+      leetcode: "https://leetcode.com/u/AnshikaGoel11/",
+      codeforces: "https://codeforces.com/profile/AnshikaGoel10",
+    },
+    education: [
+      {
+        institution: "Ajay Kumar Garg Engineering College",
+        location: "Ghaziabad, Uttar Pradesh",
+        degree: "Bachelor of Technology in Computer Science",
+        period: "2023– 2027",
+      },
+    ],
+    experience: [
+      {
+        company: "Blockchain Research Lab",
+        position: "Traniee",
+        location: "Ghazibad , UttarPradesh",
+        period: "Oct 2024 – Dec 2024",
+        responsibilities: [
+          "Created an App that have functionality of Login , signUp , signIn , Reset Password and Login with Google using Flutter",
+          "Created an App that will show the stock details of US companies by implementing APIs , User can Search or directly find the name of any US company and get  all the stock details for that current date as well as for past days with the option to see  news for that particular company and App will also show the list of all Holidays for that year plus for next two years as well , used animations to inhance the user experience ",
+        ],
+      },
+    ],
+    projects: [
+      {
+        name: "Sneakers Website Clone",
+        technologies: "Html,CSS",
+        period: "September 2024",
+        details: ["Website with asthestic sneakers for both men and women ", "resposive with the good UI"],
+      },
+      {
+        name: "Instagram Clone",
+        technologies: "html,CSS",
+        period: "Jan 2023",
+        details: ["Clone of Home page of Social media app INSTAGRAM",],
+      },
+      {
+        name: "To-Do List",
+        technologies: "html,CSS,JS",
+        period: "March 2023",
+        details: ["List in which you can add your task", "along with the functionality to delete the task ,you can edit and then save it as well", "expand its size as you increase no. of tasks"],
+      },
+      {
+        name: "Digital StopWatch",
+        technologies: "html,CSS,JS",
+        period: "March 2023",
+        details: ["Buttons to see current time,start stopwatch, add timer is provided ,watch that will show current time", "have functionality of start,Stop,reset the timer and stopwatch",],
+      },
+     
+    ],
+    skills: {
+      languages: ["C/C++", "HTML/CSS",],
+      technologies: ["Github",],
+      tools: ["VS code", "Android Studio"],
+    },
+    achievements: ["Get 1st position and Won a laptop in drawing competition among 500+ participants", "Get 50 days batch on Leetcode"],
+  };
