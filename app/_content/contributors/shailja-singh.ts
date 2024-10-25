6d2762cf4ab65c9f16724d406e228d8f90ed2041
shailja-07 <2004shailja@gmail.com>
2024-10-25 11:40:00 +0530

    added_resume

--- app/_content/contributors/shailja-singh.ts
+++ app/_content/contributors/shailja-singh.ts
@@ -1,51 +1,53 @@
 export const data = {
-    name: "Shailja Singh",
-    location: "Ghazibad ,India",
-    phone: "6387908089",
-    email: "shailja23164039@akgec.ac.in",
-    links: {
-      linkedin: "https://www.linkedin.com/in/shailja-singh-b96542334/",
-      github: "https://github.com/shailja-07",
-      leetcode: "https://leetcode.com/u/shailja-07/",
+  name: "Shailja Singh",
+  location: "Ghazibad ,India",
+  phone: "6387908089",
+  email: "shailja23164039@akgec.ac.in",
+  links: {
+    linkedin: "https://www.linkedin.com/in/shailja-singh-b96542334/",
+    github: "https://github.com/shailja-07",
+    leetcode: "https://leetcode.com/u/shailja-07/",
+    codeforces: " ",
+  },
+  education: [
+    {
+      institution: "Ajay Kumar Garg Engineering College",
+      location: "Ghaziabad, Uttar Pradesh",
+      degree: "Bachelor of Technology in Artificial Intelligence and Machine Learning",
+      period: "2023– 2027",
     },
-    education: [
-      {
-        institution: "Ajay Kumar Garg Engineering College",
-        location: "Ghaziabad, Uttar Pradesh",
-        degree: "Bachelor of Technology in Artificial Intelligence and Machine Learning",
-        period: "2023– 2027",
-      },
-    ],
-    experience: [
-      {
-        company: "Blockchain Research Lab",
-        position: "Traniee",
-        location: "Ghazibad , UttarPradesh",
-        period: "Oct 2024 – Dec 2024",
-        responsibilities: [
-          "Performed EDA on a heart disease dataset",
-          "Created a linear regression model for prediction of heart diseases ",
-        ],
-      },
-    ],
-    projects: [
-      {
-        name: "EDA",
-        technologies: "Python libraries",
-        period: "September 2024",
-        details: ["visual and exploratory analysis of data"],
-      },
-      {
-        name: "Linear regression model",
-        technologies: "streamlit",
-        period: "october 2024",
-        details: ["predict whether a person suffer from benign and malignant condition."],
-      },
-     
-    ],
-    skills: {
-      languages: ["C/C++", "HTML/CSS","python"],
-      technologies: ["Github"],
-      tools: ["VS code", "jupiter notebook"],
+  ],
+  experience: [
+    {
+      company: "Blockchain Research Lab",
+      position: "Traniee",
+      location: "Ghazibad , UttarPradesh",
+      period: "Oct 2024 – Dec 2024",
+      responsibilities: [
+        "Performed EDA on heart disease database",
+        "Created an model to predict whether a person has benign or malignant condition",
+      ],
     },
-  };
+  ],
+  projects: [
+    {
+      name: "EDA",
+      technologies: "Python",
+      period: "September 2024",
+      details: ["performed EDA on heart disease dataset."],
+    },
+    {
+      name: "heart disease prediction model",
+      technologies: "streamlit",
+      period: "oct 2024",
+      details: ["created a model to predict whether a person has benign or malignant condition",],
+    },
+   
+  ],
+  skills: {
+    languages: ["C/C++", "HTML/CSS","python",],
+    technologies: ["Github",],
+    tools: ["VS code", "jupiter notebook",],
+  },
+  achievements: ["  "],
+};
